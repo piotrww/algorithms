2ef15fc2628be5ef4da5dc16309012f26dfef370
Piotr <piotr.warsza@list.pl>
2016-05-17 12:19:28 +0200

    Lotto works!

--- lotto2.js
+++ lotto2.js
@@ -3,7 +3,7 @@ console.log("works l2");
 //Another solution
     //Lets print all possible numbers
     var allPossibleNumbers = [];
-        for(var i = 1; i <= 9; i++) {
+        for(var i = 1; i <= 49; i++) {
              allPossibleNumbers.push(i);
         }
     console.log(allPossibleNumbers);
@@ -16,8 +16,13 @@ console.log("works l2");
         var oneLuckyNumberIndex = allPossibleNumbers.indexOf(oneLuckyNumber); //Find a index in big array
         allPossibleNumbers.splice(oneLuckyNumberIndex, 1); //Delete content of this index in big array
         sixLuckyNumbers.push(oneLuckyNumber); //Pushing a number to a array with lucky numbers.
-        console.log(oneLuckyNumber + "-<number lucky. " + allPossibleNumbers + " Deleting a drawing number, time: " + j)
+        console.log(oneLuckyNumber + "-<number lucky. " + allPossibleNumbers + " Deleted a lucky number, nr: " + j)
                //Why no array here?
     }
-    console.log(sixLuckyNumbers);
-    console.log(allPossibleNumbers);
+  
+    function giveIncreasingOrder(arr) {
+        var sortedSixLuckyNumbers = arr.sort();
+        return sortedSixLuckyNumbers;
+    }
+    console.log(giveIncreasingOrder(sixLuckyNumbers));
+    console.log("Array without lucky numbers " + allPossibleNumbers);
